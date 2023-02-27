4ce9945443e5a2bdab21022d1a70967df4930edf
Marcelo Gomes <marcs.rgo@gmail.com>
2023-02-26 22:13:28 -0300

    adding heroky

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express =  require('express');
 const app = express();
+const path = require("push");
 
-app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use("/assets/css", express.static(__dirname + '/assets/css'));
 app.use("/assets/js", express.static(__dirname + '/assets/js'));
 app.use("/assets/img", express.static(__dirname + '/assets/img'));
 app.use("/assets/fonts", express.static(__dirname + '/assets/fonts'));
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
     res.render("index");
 })
 
-app.listen(8080)
+const server = app.listen(process.env.PORT || 8080);
+const partNumber = server.address().port;
